a56d55243a15301bd551711a756b7a125b639a91
Mindyc123 <MIndyc14@nycstudents.net>
2024-09-10 17:33:11 -0400

    Lab 1

---
+++ Chen_Mindy_lab_1.cpp
@@ -0,0 +1,53 @@
+//Mindy Chen
+//Lab 1
+//9.10.24
+
+//***************************************************************************************************************
+//For this lab, we used the cout command to replicate the output given to us and added a wall of hashtags around 
+//***************************************************************************************************************
+
+#include <iostream> 
+
+//The #include <iostrean> is used to start every C++ program 
+
+using namespace std;
+
+//The using namespace std command allows us to exclude the prefix std when using the cout and endl command 
+
+int main()
+
+//The int main() command makes the function main which is required in all C++ programs 
+
+{
+
+//I copied and pasted the output given to us and added an cout and endl command in front and back each line
+
+    cout << "######################################################################################" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Press the key combination Ctrl + Shift + Escape                         #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#            Type “Task Manager” in the Windows Start menu search                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "######################################################################################" << endl;
+
+//For the box, I just added hashtags to the side in the cout and endl commands cotaining the original output 
+
+    return 0;
+}
+
+
+//######################################################################################
+//#                                                                                    #
+//# Ways to access the Task Manager on your Windows computer:                          #
+//#                                                                                    #
+//#            Press the key combination Ctrl + Shift + Escape                         #
+//#                                                                                    #
+//#            Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+//#                                                                                    #
+//#            Type “Task Manager” in the Windows Start menu search                    #
+//#                                                                                    #
+//######################################################################################
